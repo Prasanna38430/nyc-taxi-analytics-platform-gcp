4a4fbd4784fd1a56a668270334346c54a3029908
Prasanna38430 <prasannakumar38430@gmail.com>
2026-04-11 16:03:25 +0200

    fix(terraform): Reference existing firewall rule as data source

--- infrastructure/terraform/firewall.tf
+++ infrastructure/terraform/firewall.tf
@@ -1,27 +1,4 @@
-# Custom Firewall Rule for Dataproc Internal Communication
-resource "google_compute_firewall" "allow_dataproc_internal" {
-  name    = "allow-dataproc-internal"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["0-65535"]
-  }
-
-  allow {
-    protocol = "udp"
-    ports    = ["0-65535"]
-  }
-
-  allow {
-    protocol = "icmp"
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  priority      = 1000
-
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes = all
-  }
+# Reference existing firewall rule (already created)
+data "google_compute_firewall" "allow_dataproc_internal" {
+  name = "allow-dataproc-internal"
 }
