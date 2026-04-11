6e00a32362c140b560e0b6d064bda6b2a4dc598f
Prasanna38430 <prasannakumar38430@gmail.com>
2026-04-11 13:35:16 +0200

    fix(terraform): Use google_storage_bucket_object resource type instead of google_storage_object

--- infrastructure/terraform/storage.tf
+++ infrastructure/terraform/storage.tf
@@ -19,56 +19,56 @@ resource "google_storage_bucket" "data_bucket" {
 }
 
 # Raw Data Folder
-resource "google_storage_object" "raw_folder" {
+resource "google_storage_bucket_object" "raw_folder" {
   name   = "raw/"
   bucket = google_storage_bucket.data_bucket.name
   content = " "
 }
 
 # Raw Taxi Trips Folder
-resource "google_storage_object" "raw_taxi_trips_folder" {
+resource "google_storage_bucket_object" "raw_taxi_trips_folder" {
   name   = "raw/taxi_trips/"
   bucket = google_storage_bucket.data_bucket.name
   content = " "
 }
 
 # Composer DAGs Folder
-resource "google_storage_object" "composer_dags_folder" {
+resource "google_storage_bucket_object" "composer_dags_folder" {
   name   = "composer/dags/"
   bucket = google_storage_bucket.data_bucket.name
   content = " "
 }
 
 # Spark Scripts Folder
-resource "google_storage_object" "spark_scripts_folder" {
+resource "google_storage_bucket_object" "spark_scripts_folder" {
   name   = "scripts/spark/"
   bucket = google_storage_bucket.data_bucket.name
   content = " "
 }
 
 # ML Artifacts Folder
-resource "google_storage_object" "ml_artifacts_folder" {
+resource "google_storage_bucket_object" "ml_artifacts_folder" {
   name   = "ml/artifacts/"
   bucket = google_storage_bucket.data_bucket.name
   content = " "
 }
 
 # ML Models Folder
-resource "google_storage_object" "ml_models_folder" {
+resource "google_storage_bucket_object" "ml_models_folder" {
   name   = "ml/models/"
   bucket = google_storage_bucket.data_bucket.name
   content = " "
 }
 
 # Temp Spark Folder
-resource "google_storage_object" "temp_spark_folder" {
+resource "google_storage_bucket_object" "temp_spark_folder" {
   name   = "temp/spark_temp/"
   bucket = google_storage_bucket.data_bucket.name
   content = " "
 }
 
 # Terraform State Folder
-resource "google_storage_object" "terraform_folder" {
+resource "google_storage_bucket_object" "terraform_folder" {
   name   = "terraform/"
   bucket = google_storage_bucket.data_bucket.name
   content = " "
