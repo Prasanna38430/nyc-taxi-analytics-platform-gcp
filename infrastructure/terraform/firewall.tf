1d1174867ea2b78548ad51a527c4bf36d4b4604c
Prasanna38430 <prasannakumar38430@gmail.com>
2026-04-11 08:00:15 +0200

    Phase 9: Add firewall.tf - Network security rules
    Creates firewall rule:
    - allow-dataproc-internal
      ✓ Allows TCP (all ports)
      ✓ Allows UDP (all ports)
      ✓ Allows ICMP
      ✓ Priority: 1000
      ✓ Network: default
    
    Enables internal Dataproc cluster communication

---
+++ infrastructure/terraform/firewall.tf
@@ -0,0 +1,24 @@
+# Custom Firewall Rule for Dataproc Internal Communication
+resource "google_compute_firewall" "allow_dataproc_internal" {
+  name    = "allow-dataproc-internal"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  priority      = 1000
+
+  labels = local.common_labels
+}
